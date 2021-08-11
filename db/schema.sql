c7769377e79784c784cf90c1990ad77a1cb0bf4b
Your Name <robertcjohnson1984@gmail.com>
2021-08-10 22:00:45 -0600

    create department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,11 @@ CREATE database employeeTrackerDB;
 Use employeeTrackerDB;
 
 -- department
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
 
 -- role
 
