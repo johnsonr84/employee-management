99b58f8950ba3b841ec7559a23cce926f613ee8b
Your Name <robertcjohnson1984@gmail.com>
2021-08-10 21:04:26 -0600

    add node.js driver for mysql

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer')
 const consoleTable = require('console.table');
+
+// node.js driver for mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employeetrackerdb',
+});
+
+
+
