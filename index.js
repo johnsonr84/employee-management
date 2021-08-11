d1442e5f4a3b04a9308ab7da46ba049652a8cf56
Your Name <robertcjohnson1984@gmail.com>
2021-08-10 22:02:21 -0600

    add db pass

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'password',
     database: 'employeetrackerdb',
 });
 
