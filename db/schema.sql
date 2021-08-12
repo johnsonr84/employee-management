f8bee14113902fc6fbb9a681fc7d2d0a0bc9bba4
Your Name <robertcjohnson1984@gmail.com>
2021-08-11 22:52:18 -0600

    update schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,33 @@
-CREATE database employeeTrackerDB;
+
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+
 Use employeeTrackerDB;
 
--- department
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
--- role
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
 
--- employee 
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+
+
+
