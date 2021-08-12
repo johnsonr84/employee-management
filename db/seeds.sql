b564d85210414134b194ef4ac02ec17352c12d59
Your Name <robertcjohnson1984@gmail.com>
2021-08-11 22:54:51 -0600

    add password to connection and fix bd typo

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-USE employeeTrackerDB;
-
-INSERT INTO department (id, name)
-VALUES (1, 'Management'), (2, 'Accounting'), (3, 'Sales'), (4, 'Marketing');
--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: 'password',
-    database: 'employeetrackerdb',
+    database: 'employeeTrackerDB',
 });
 
 // establishes connection
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    connection.end();
   });
