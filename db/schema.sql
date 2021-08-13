4d3c10fdd92a1939eec5a0d9350f20522e723d40
Your Name <robertcjohnson1984@gmail.com>
2021-08-13 00:04:07 -0600

    update bd

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employeeTrackerDB;
 
 Use employeeTrackerDB;
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE roles (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
